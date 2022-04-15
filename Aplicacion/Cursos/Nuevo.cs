05efdce470306e3120f5a3f97ba19b4130061846
jm82mexico <jmdelan2012@gmail.com>
2022-04-14 20:57:40 -0500

    agregar nuevos cursos

---
+++ Aplicacion/Cursos/Nuevo.cs
@@ -0,0 +1,49 @@
+using Dominio;
+using MediatR;
+using Persitencia;
+
+namespace Aplicacion.Cursos
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string Titulo { get; set; }
+
+            public string Descripcion { get; set; }
+
+            public DateTime FechaPublicacion { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosOnlineContext context;
+
+            public Manejador(CursosOnlineContext _context)
+            {
+                context = _context;
+            }
+
+            public async Task<Unit>
+            Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var curso =
+                    new Curso {
+                        Titulo = request.Titulo,
+                        Descripcion = request.Descripcion,
+                        FechaPublicacion = request.FechaPublicacion
+                    };
+
+                context.Curso.Add (curso);
+                var valor = await context.SaveChangesAsync();
+
+                if (valor > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo insertar el curso");
+            }
+        }
+    }
+}
--- WebAPI/Controllers/CursosController.cs
+++ WebAPI/Controllers/CursosController.cs
@@ -29,5 +29,11 @@ namespace WebAPI.Controllers
         {
             return await mediator.Send(new ConsultaId.CursoUnico { Id = id });
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data)
+        {
+            return await mediator.Send(data);
+        }
     }
 }
